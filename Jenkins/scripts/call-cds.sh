d7974776f96b3569c5aba8f5e9ff29e70e3528ca
ValeriaCalzada <115190479+ValeriaCalzada@users.noreply.github.com>
2024-09-03 14:52:03 -0600

    add withCredentials

--- Jenkins/scripts/call-cds.sh
+++ Jenkins/scripts/call-cds.sh
@@ -7,7 +7,7 @@ TOKEN_URL="https://iam-svc.dms.uset2.ficoanalyticcloud.com/openam/oauth2/access_
 body="client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials&realm=/FicoAnalyticCloud"
 echo $body
 
-response=$(curl -H "Content-Type: application/x-www-form-urlencoded" \
+response=$(curl -H "Content-Type: application/x-www-form-urlencoded" -H "Accept-Charset: UTF-8" \
         -X POST \
         -d "$body" \
         "$TOKEN_URL")
