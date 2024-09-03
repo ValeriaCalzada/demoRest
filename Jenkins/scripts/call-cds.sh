0732409fc9436521bc775beadcdee4389fea3c9e
ValeriaCalzada <115190479+ValeriaCalzada@users.noreply.github.com>
2024-09-03 12:54:17 -0600

    Update call-cds.sh

--- Jenkins/scripts/call-cds.sh
+++ Jenkins/scripts/call-cds.sh
@@ -4,9 +4,9 @@ TOKEN_URL="https://iam-svc.dms.uset2.ficoanalyticcloud.com/openam/oauth2/access_
 
 body="client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials&realm=/FicoAnalyticCloud"
 
-response = $(curl -H "Content-Type: application/x-www-form-urlencoded" \
+response = curl -H "Content-Type: application/x-www-form-urlencoded" \
         -X POST \
         -d "$body" \
-        "$TOKEN_URL")
+        "$TOKEN_URL"
 
 echo $response
