3e0854b6ff8dfd82807632caff0cd93c068e5503
ValeriaCalzada <115190479+ValeriaCalzada@users.noreply.github.com>
2024-09-03 14:42:18 -0600

    Update call-cds.sh

--- Jenkins/scripts/call-cds.sh
+++ Jenkins/scripts/call-cds.sh
@@ -1,6 +1,9 @@
+
+
 #!/bin/bash
 
 TOKEN_URL="https://iam-svc.dms.uset2.ficoanalyticcloud.com/openam/oauth2/access_token" 
+echo $CLIENT_ID
 
 body="client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials&realm=/FicoAnalyticCloud"
 
