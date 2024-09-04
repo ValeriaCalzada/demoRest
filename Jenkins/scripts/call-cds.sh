7bb1c28c2061b314d790cc906a8a6fd068a2ad8b
ValeriaCalzada <115190479+ValeriaCalzada@users.noreply.github.com>
2024-09-04 17:30:51 -0600

    Update call-cds.sh

--- Jenkins/scripts/call-cds.sh
+++ Jenkins/scripts/call-cds.sh
@@ -7,13 +7,14 @@ TOKEN_URL=$TOKEN_URL
 
 body="client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials&realm=/FicoAnalyticCloud"
 
-response=$(curl -H "Content-Type: application/x-www-form-urlencoded" -H "Accept-Charset: UTF-8" \
+responseAuth=$(curl -H "Content-Type: application/x-www-form-urlencoded" -H "Accept-Charset: UTF-8" \
         -X POST \
         -d "$body" \
         "$TOKEN_URL")
 
-ENCODED_TOKEN=$(echo -n "$response.access_token" | base64)
+ENCODED_TOKEN=$(echo -n "$responseAuth.access_token" | base64)
 
-curl  -v --http1.1 -H "Content-Type: application/json" -H "Authorization: Bearer ${ENCODED_TOKEN}"\
+responseSession=$(curl --http1.1 -H "Content-Type: application/json" -H "Authorization: Bearer ${ENCODED_TOKEN}"\
         -X POST \
-        "$CDS_INIT_URL"
+        "$CDS_INIT_URL")
+echo $responseSession
