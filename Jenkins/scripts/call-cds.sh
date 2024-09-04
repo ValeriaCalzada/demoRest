8e7c0739959ce8aaeb2bc8dcf27ca7ccc7f93e63
ValeriaCalzada <115190479+ValeriaCalzada@users.noreply.github.com>
2024-09-04 17:33:13 -0600

    Add brackets to echo responseSession

--- Jenkins/scripts/call-cds.sh
+++ Jenkins/scripts/call-cds.sh
@@ -17,4 +17,4 @@ ENCODED_TOKEN=$(echo -n "$responseAuth.access_token" | base64)
 responseSession=$(curl --http1.1 -H "Content-Type: application/json" -H "Authorization: Bearer ${ENCODED_TOKEN}"\
         -X POST \
         "$CDS_INIT_URL")
-echo $responseSession
+echo "${responseSession}"
