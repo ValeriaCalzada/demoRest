6d10009c67ee791493a7064a2120a8a75c3fae3c
ValeriaCalzada <115190479+ValeriaCalzada@users.noreply.github.com>
2024-09-05 12:02:04 -0600

    Update call-cds.sh

--- Jenkins/scripts/call-cds.sh
+++ Jenkins/scripts/call-cds.sh
@@ -5,20 +5,26 @@
 #cambiar a env var in jenkins
 TOKEN_URL=$TOKEN_URL 
 
-body="client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials&realm=/FicoAnalyticCloud"
+bodyAuth="client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials&realm=/FicoAnalyticCloud"
 
 responseAuth=$(curl -H "Content-Type: application/x-www-form-urlencoded" -H "Accept-Charset: UTF-8" \
         -X POST \
-        -d "$body" \
+        -d "$bodyAuth" \
         "$TOKEN_URL")
 
 access_token=$(echo "$responseAuth" | jq -r '.access_token')
 ENCODED_TOKEN=$(echo -n "$access_token" | base64)
 responseSession=$(curl -H "Content-Type: application/json" -H "Authorization: Bearer ${ENCODED_TOKEN}" \
         -X GET \
-        -c - "$CDS_INIT_URL" -w '%{http_code}')
+        -c - "$GATEWAY_URL/init" -w '%{http_code}')
 
 jSessionId=$(echo "$responseSession" | grep 'JSESSIONID'| awk '{print $7}')
+XSRFTOKEN=$(echo "$responseSession" | grep 'XSRFTOKEN'| awk '{print $7}')
 
+bodyApplication='{"payload": "{\"device\":{\"deviceId\":\"2\",\"ipAddress\":\"1.127.0.1\"},\"applicationId\":\"12\"}"}'
+responseCDS=$(curl -H "Content-Type: application/json" -H "Authorization: Bearer ${ENCODED_TOKEN}" -H "X-XSRF-TOKEN: ${XSRFTOKEN}" \
+        -X GET \
+        -d "$bodyApplication" \
+        "$GATEWAY_URL/feature-store-adapter-service/v2/data/Application")
 
-echo "La respuesta es: ${jSessionId}"
+echo "La respuesta es: ${responseCDS}"
