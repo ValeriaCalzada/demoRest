033ff0c5a2890e3cd69e1837919cec5ba184f246
ValeriaCalzada <115190479+ValeriaCalzada@users.noreply.github.com>
2024-09-05 11:26:45 -0600

    Update call-cds.sh

--- Jenkins/scripts/call-cds.sh
+++ Jenkins/scripts/call-cds.sh
@@ -16,6 +16,6 @@ access_token=$(echo "$responseAuth" | jq -r '.access_token')
 ENCODED_TOKEN=$(echo -n "$access_token" | base64)
 responseSession=$(curl -H "Content-Type: application/json" -H "Authorization: Bearer ${ENCODED_TOKEN}" \
         -X GET \
-        -c "$CDS_INIT_URL" -w '%{http_code}')
+        -c - "$CDS_INIT_URL" -w '%{http_code}')
 
 echo "La respuesta es: ${responseSession}"
