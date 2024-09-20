789997e0226f535b201c2b7580554bba1ba4b7f1
ValeriaCalzada <115190479+ValeriaCalzada@users.noreply.github.com>
2024-09-20 17:37:42 -0600

    syntax: add ${WORKSPACE} var

--- Jenkins/scripts/extract-scripts.sh
+++ Jenkins/scripts/extract-scripts.sh
@@ -4,13 +4,13 @@
     echo "Processing $shJob..."
 
     # First extraction (e.g., extracting from <parameter>)
-    xmlstarlet sel -t -m "//parameter/value" -v . -n "./Jobs/$shJob" > "extracted_value_param_from_${shJob}.js"
+    xmlstarlet sel -t -m "//parameter/value" -v . -n "${WORKSPACE}/Jobs/$shJob" > "extracted_value_param_from_${shJob}.js"
 
     # Second extraction (e.g., extracting from <custom-function-script>)
-    xmlstarlet sel -t -m "//custom-function-script" -v . -n "./Jobs/$shJob" > "extracted_script_from_${shJob}.js"
+    xmlstarlet sel -t -m "//custom-function-script" -v . -n "${WORKSPACE}/Jobs/$shJob" > "extracted_script_from_${shJob}.js"
 
-    xmlstarlet sel -t -m "//inline-script" -v . -n "./Jobs/$shJob" > "extracted_script_from_${shJob}.py"
+    xmlstarlet sel -t -m "//inline-script" -v . -n "${WORKSPACE}/Jobs/$shJob" > "extracted_script_from_${shJob}.py"
 
     # Add more extractions as needed
     # For example, extracting some <generated-field> tags:
-    xmlstarlet sel -t -m "//function-script" -v . -n "./Jobs/$shJob" > "extracted_function-script_from_${shJob}.js"
+    xmlstarlet sel -t -m "//function-script" -v . -n "${WORKSPACE}/Jobs/$shJob" > "extracted_function-script_from_${shJob}.js"
