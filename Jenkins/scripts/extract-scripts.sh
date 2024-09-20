8e9e895d544b7807f3299aaaae0983de4e88952c
ValeriaCalzada <115190479+ValeriaCalzada@users.noreply.github.com>
2024-09-20 17:29:29 -0600

    syntax: variable job change

--- Jenkins/scripts/extract-scripts.sh
+++ Jenkins/scripts/extract-scripts.sh
@@ -1,16 +1,16 @@
 
 # Loop through each job and run different xmlstarlet commands
 
-    echo "Processing $job..."
+    echo "Processing $shJob..."
 
     # First extraction (e.g., extracting from <parameter>)
-    xmlstarlet sel -t -m "//parameter/value" -v . -n "Jobs/$job" > "extracted_value_param_from_${job}.js"
+    xmlstarlet sel -t -m "//parameter/value" -v . -n "Jobs/$shJob" > "extracted_value_param_from_${shJob}.js"
 
     # Second extraction (e.g., extracting from <custom-function-script>)
-    xmlstarlet sel -t -m "//custom-function-script" -v . -n "Jobs/$job" > "extracted_script_from_${job}.js"
+    xmlstarlet sel -t -m "//custom-function-script" -v . -n "Jobs/$shJob" > "extracted_script_from_${shJob}.js"
 
-    xmlstarlet sel -t -m "//inline-script" -v . -n "Jobs/$job" > "extracted_script_from_${job}.py"
+    xmlstarlet sel -t -m "//inline-script" -v . -n "Jobs/$shJob" > "extracted_script_from_${shJob}.py"
 
     # Add more extractions as needed
     # For example, extracting some <generated-field> tags:
-    xmlstarlet sel -t -m "//function-script" -v . -n "Jobs/$job" > "extracted_function-script_from_${job}.js"
+    xmlstarlet sel -t -m "//function-script" -v . -n "Jobs/$shJob" > "extracted_function-script_from_${shJob}.js"
